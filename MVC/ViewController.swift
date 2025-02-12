21257199c1b861ce095acb5af342a3aa395500e5
Duc Nguyen <>
2025-02-12 21:46:50 +0700

    init project

---
+++ MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Admin on 12/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
